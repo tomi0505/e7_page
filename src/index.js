f45a3f706f1ecc72b2cf54adc14f4a7445bfc499
tomi0505 <tomaszgrzyb.online@gmail.com>
2020-12-28 19:49:34 +0100

    'done'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import $ from 'jquery';
+window.$ = window.jQuery = $;
 import 'bootstrap';
 import './scss/style.scss';
 
-// document.addEventListener("DOMContentLoaded", function() {}, false);
+// document.addEventListener("DOMContentLoaded", () => {});
