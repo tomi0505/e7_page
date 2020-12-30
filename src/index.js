0d33ce838b7add2e0d448e524121f0afc3132544
tomi0505 <tomaszgrzyb.online@gmail.com>
2020-12-30 14:26:41 +0100

    project 82%

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './scss/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const changeLanguageBtnItems = document.querySelectorAll('.nav-b .dropdown-menu .dropdown-item');
+  for(let item of changeLanguageBtnItems) {
+    item.addEventListener('click', function() {
+      document.documentElement.lang = this.dataset.language;
+    });
+  }
+});
