22cf214fb6fa16bee50c83f3db944811c0602239
tomi0505 <tomaszgrzyb.online@gmail.com>
2021-01-01 14:37:05 +0100

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import './scss/style.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
+const languages = [
+  {
+    name: 'polski',
+    shortName: 'pl',
+    flagPath: 'flag-pl.svg'
+  },
+  {
+    name: 'angielski',
+    shortName: 'en',
+    flagPath: 'flag-usa.svg'
+  },
+  {
+    name: 'rosyjski',
+    shortName: 'ru',
+    flagPath: 'flag-russia.svg'
+  }
+];
+
+const changePageLanguage = languages => {
   const changeLanguageBtnItems = document.querySelectorAll('.nav-b .dropdown-menu .dropdown-item');
+  const languageNameEl = document.querySelector('.nav-b .change-language-btn-b__language-name');
+  const countryFlagUseEl = document.querySelector('.change-language-btn-b__country-flag-use');
+
   for(let item of changeLanguageBtnItems) {
     item.addEventListener('click', function() {
-      document.documentElement.lang = this.dataset.language;
+      const lastLanguage = languageNameEl.textContent.toLowerCase();
+      const lastLanguageName = languages.find(item => item.shortName === lastLanguage).name;
+      const selectedLanguage = this.dataset.language;
+      const selectedLanguageFlagPath = languages.find(item => item.shortName === selectedLanguage).flagPath;
+
+      document.documentElement.lang = selectedLanguage;
+      languageNameEl.textContent = selectedLanguage.toUpperCase();
+
+      this.dataset.language = lastLanguage;
+      this.textContent = lastLanguageName;
+
+      countryFlagUseEl.setAttribute('xlink:href', `src/img/${selectedLanguageFlagPath}#flag`);
     });
   }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  changePageLanguage(languages);
 });
